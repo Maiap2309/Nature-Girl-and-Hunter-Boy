df71aa992ad536177f92f4284075beb034108f7f
Maia Piomelli <maiapiomelli@gmail.com>
2020-07-20 13:09:54 -0700

    Change

---
+++ Assets/scripts/ItalyMoveG.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItalyMoveG : MonoBehaviour
+{
+    public float speed;
+    public bool playerIsOnTheGround = true;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Transform>().position -= new Vector3(0, speed, 0) * Time.deltaTime;
+            rotateMove(180);
+            GetComponent<Animator>().SetBool("Move", true);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Transform>().position += new Vector3(0,speed,0) * Time.deltaTime;
+            rotateMove(0);
+
+            GetComponent<Animator>().SetBool("Move", true);
+        }
+        else
+        {
+            GetComponent<Animator>().SetBool("Move", false);
+        }
+        if (Input.GetKeyDown(KeyCode.W) && playerIsOnTheGround)
+            GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpForce, 0), ForceMode.Impulse);
+
+
+
+    }
+
+    private void rotateMove(float dir)
+    {
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, dir, transform.eulerAngles.z);
+        print(dir);
+    }
+
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+            playerIsOnTheGround = true;
+    }
+
+
+    private void OnCollisionExit(Collision collision)
+
+    {
+        if (collision.gameObject.tag == "Ground")
+            playerIsOnTheGround = false;
+    }
+}
+
+
